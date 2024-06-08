30f49c231d684bdb56be859ec84d6c4689eabb08
TuanKhoii <165405752+TuanKhoii@users.noreply.github.com>
2024-06-08 19:02:44 +0700

    A+B.cpp

---
+++ A+B.cpp
@@ -0,0 +1,14 @@
+/*
+Tính  a + b
+Input
+Nhập vào 2 số nguyên a và b.
+Output
+xuất ra tổng của 2 số a và b
+*/
+#include <iostream>
+int main() {
+    int a , b;
+    std::cin >> a >> b;
+    std::cout << a + b;
+    return 0;
+}
